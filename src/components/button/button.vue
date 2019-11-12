5fe527027f0060548f838ce4ebc93aff700ecb3a
yinhang <yinhang@dankegongyu.com>
2019-11-12 17:28:33 +0800

    ✨ Feat:

---
+++ src/components/button/button.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button class="my-button">
+       <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'button',
+  props: {
+    type: {
+      type: String,
+      default: 'primary'
+    },
+    size: {
+      type: String,
+      default: 'middle'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
