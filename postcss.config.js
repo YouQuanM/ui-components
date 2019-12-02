3030601967cd9dc0c81f5abd4adf6e2a77e98e7f
yinhang <yinhang@dankegongyu.com>
2019-12-02 20:39:48 +0800

    🐛 Fix:

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,7 @@ module.exports = {
     "postcss-pxtorem": {
        // If you modify rootValue, you also need to modify rem in static/responsive.js
        rootValue: 100, // 1rem=100px
-       propWhiteList: ['*','!border'],
+       // propWhiteList: ['*','!border'],
        // minPixelValue: '1px', //设置小于多少尺寸将不会进行转换。
        selectorBlackList: ['.px-'] // 过滤掉.px-开头的class，不进行rem转换
     }
