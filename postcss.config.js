95bbc9282288283297a85c378f4bb7b734a6dd98
yinhang <yinhang@dk-yinhangdeMacBook-Pro.local>
2019-12-02 10:35:25 +0800

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {}
+  "plugins": {
+    "postcss-import": {},
+    "postcss-url": {},
+    "autoprefixer": {},
+    "postcss-pxtorem": {
+      // If you modify rootValue, you also need to modify rem in static/responsive.js
+      rootValue: 100, // 1rem=100px
+      propWhiteList: ['*','!border'],
+      // minPixelValue: '1px', //设置小于多少尺寸将不会进行转换。
+      selectorBlackList: ['.px-'] // 过滤掉.px-开头的class，不进行rem转换
+    }
   }
 }
