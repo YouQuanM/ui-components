4ad2a214faa321896f48579bf3e39c1331a6829e
yinhang <yinhang@dankegongyu.com>
2019-12-05 11:22:40 +0800

    add switch

---
+++ src/package/switch/switch.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="my-switch">
+    <input type="checkbox" v-model="value">
+  </div>
+</template>
+
+<script>
+const COMPONENT_NAME = 'my-radio'
+export default {
+  name: COMPONENT_NAME
+}
+</script>
